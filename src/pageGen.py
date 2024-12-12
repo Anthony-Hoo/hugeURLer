68d333ace8070086dfb770ff4d5799111cb135a1
powerfullz <wleizc@hotmail.com>
2024-12-12 23:43:04 +0800

    fix vercel 404 error

--- src/pageGen.py
+++ src/pageGen.py
@@ -1,8 +1,11 @@
+from os import mkdir
+
 def pageGen(identifier, url):
     template = ""
     with open("./template/template.html", "r", encoding="utf-8") as f:
         template = f.read()
     template = template.replace("[/title/]", identifier)
     template = template.replace("[/url/]", url)
-    with open("./pages/" + identifier + ".html", "w", encoding="utf-8") as f:
+    mkdir("./pages/" + identifier)
+    with open("./pages/" + identifier + "/index.html", "w", encoding="utf-8") as f:
         f.write(template)
